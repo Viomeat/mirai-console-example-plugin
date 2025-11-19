447f279171e766cf2707360fb111307ae5edf24f
Viomeat <151036798+Viomeat@users.noreply.github.com>
2025-11-19 17:55:44 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,8 @@
 pluginManagement {
     repositories {
         mavenLocal()
+        mavenCentral()
         gradlePluginPortal()
-        jcenter()
-        maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
 }
 rootProject.name = "mirai-console-example-plugin"
