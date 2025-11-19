e8a244f3b59224587566f7922fb20a89db2c9712
Viomeat <151036798+Viomeat@users.noreply.github.com>
2025-11-19 17:58:25 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    val kotlinVersion = "1.9.22"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.16.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    `maven-publish`
+}
+
+mirai {
+    coreVersion = "2.16.0"
+}
+
 group = "org.example"
 version = "0.1.0"
 
